e1e14d831e4bc58d0f87c3fbf375e673a4d2fe14
baguidevos <statamjason2020@gmail.com>
2026-08-29 15:40:51 +0000

    feat: ajout du seeder pour le compte webmaster@gmail.com

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use LaravelDaily\FilaTeams\Models\Team;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $webmaster = User::firstOrCreate(
+            ['email' => 'webmaster@gmail.com'],
+            [
+                'name' => 'Webmaster',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (! $webmaster->is_admin) {
+            $webmaster->update(['is_admin' => true]);
+        }
+
+        if ($webmaster->teams()->count() === 0) {
+            $team = Team::firstOrCreate(
+                ['slug' => 'webmaster-team'],
+                [
+                    'name' => "Webmaster's Team",
+                    'is_personal' => true,
+                ]
+            );
+
+            if (! $webmaster->belongsToTeam($team)) {
+                $webmaster->teams()->attach($team->id, ['role' => 'owner']);
+            }
+
+            $webmaster->update(['current_team_id' => $team->id]);
+        }
     }
 }
