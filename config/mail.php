8f307a477186483f8a4d7bac0e6eb1a4d466c9f1
baguidevos <statamjason2020@gmail.com>
2026-08-29 22:29:36 +0000

    fix: normalisation du nom du mailer et nettoyage des jobs de queue obsolètes

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => strtolower((string) env('MAIL_MAILER', 'log')),
 
     /*
     |--------------------------------------------------------------------------
